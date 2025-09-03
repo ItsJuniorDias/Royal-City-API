65f632accf84000547c831b29175f847191488fa
Alexandre <reabilitado97@gmail.com>
2025-09-03 16:00:37 -0300

    chore: key production stripe

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "https://omnistate.vercel.app", // your frontend URL
+    origin: "https://omnistate.onrender.com", // your frontend URL
     credentials: true, // if you want to send cookies/auth headers
   })
 );
